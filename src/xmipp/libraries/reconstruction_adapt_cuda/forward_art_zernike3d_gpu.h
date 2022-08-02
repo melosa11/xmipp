e6561884d1be17c902633569942b84a647ec6e90
Jakub Kiska <kubo.kiska@gmail.com>
2022-08-02 16:01:04 +0200

    Remove extra qualification in header

--- src/xmipp/libraries/reconstruction_adapt_cuda/forward_art_zernike3d_gpu.h
+++ src/xmipp/libraries/reconstruction_adapt_cuda/forward_art_zernike3d_gpu.h
@@ -242,7 +242,7 @@ public:
      *
      * Bilinear interpolation. (x,y) are in logical coordinates.
      */
-    PrecisionType ProgForwardArtZernike3DGPU::interpolatedElement2DCuda(double x, double y, MultidimArrayCuda<PrecisionType> &diffImage);
+    PrecisionType interpolatedElement2DCuda(double x, double y, MultidimArrayCuda<PrecisionType> &diffImage) const;
 
     void backwardModel(bool usesZernike);
 
