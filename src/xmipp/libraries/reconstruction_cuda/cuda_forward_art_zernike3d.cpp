da0b432e9728546c4d0e5b062db2d9d93aed77f3
Dávid Šutor <david@sutor.sk>
2022-08-03 11:01:12 +0200

    Remove unused code

--- src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
+++ src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
@@ -3,58 +3,6 @@
 // Standard includes
 #include <iostream>
 
-
-// Common functions
-template<typename T>
-cudaError cudaMallocAndCopy(T **target, const T *source, size_t numberOfElements, size_t memSize = 0) {
-    size_t elemSize = numberOfElements * sizeof(T);
-    memSize = memSize == 0 ? elemSize : memSize * sizeof(T);
-
-    cudaError err = cudaSuccess;
-    if ((err = cudaMalloc(target, memSize)) != cudaSuccess) {
-        *target = NULL;
-        return err;
-    }
-
-    if ((err = cudaMemcpy(*target, source, elemSize, cudaMemcpyHostToDevice)) != cudaSuccess) {
-        cudaFree(*target);
-        *target = NULL;
-    }
-
-    if (memSize > elemSize) {
-        cudaMemset((*target) + numberOfElements, 0, memSize - elemSize);
-    }
-
-    return err;
-}
-
-void processCudaError() {
-    cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        fprintf(stderr, "Cuda error: %s\n", cudaGetErrorString(err));
-        exit(err);
-    }
-}
-
-// Copies data from CPU to the GPU and at the same time transforms from
-// type 'U' to type 'T'. Works only for numeric types
-template<typename Target, typename Source>
-void transformData(Target **dest, Source *source, size_t n, bool mallocMem = true) {
-    std::vector <Target> tmp(source, source + n);
-
-    if (mallocMem) {
-        if (cudaMalloc(dest, sizeof(Target) * n) != cudaSuccess) {
-            processCudaError();
-        }
-    }
-
-    if (cudaMemcpy(*dest, tmp.data(), sizeof(Target) * n, cudaMemcpyHostToDevice) != cudaSuccess) {
-        processCudaError();
-    }
-}
-
-// CUDAForwardArtZernike3D methods
-
 template<typename PrecisionType>
 CUDAForwardArtZernike3D<PrecisionType>::CUDAForwardArtZernike3D(
         const CUDAForwardArtZernike3D<PrecisionType>::ConstantParameters parameters) {
