89dce3073c0aa911290442a16c8afc6a3e4f1eae
Jakub Kiska <kubo.kiska@gmail.com>
2022-08-09 12:22:25 +0200

    Rename processCudaError

--- src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
+++ src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
@@ -351,7 +351,7 @@ namespace {
         return err;
     }
 
-    void processCudaError(int i) {
+    void processCudaErrorOur() {
         cudaError_t err = cudaGetLastError();
         if (err != cudaSuccess) {
             fprintf(stderr, "Cuda error: %s\n", cudaGetErrorString(err));
@@ -367,12 +367,12 @@ namespace {
 
         if (mallocMem) {
             if (cudaMalloc(dest, sizeof(Target) * n) != cudaSuccess) {
-                processCudaError(0);
+                processCudaErrorOur();
             }
         }
 
         if (cudaMemcpy(*dest, tmp.data(), sizeof(Target) * n, cudaMemcpyHostToDevice) != cudaSuccess) {
-            processCudaError(0);
+            processCudaErrorOur();
         }
     }
     template<typename T>
@@ -385,7 +385,7 @@ namespace {
     void setupVectorOfMultidimArray(std::vector<MultidimArrayCuda<T>>& inputVector, MultidimArrayCuda<T>** outputVectorData)
     {
         if (cudaMallocAndCopy(&outputVectorData, inputVector.data(), inputVector.size()) != cudaSuccess)
-            processCudaError(0);
+            processCudaErrorOur();
     }
 
     template<typename T>
