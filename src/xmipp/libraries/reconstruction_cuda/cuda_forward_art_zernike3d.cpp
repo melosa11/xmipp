b8055b2af04b7dbb79c27f6065a1eaa1ec99ea15
Jakub Kiska <kubo.kiska@gmail.com>
2022-08-17 13:15:15 +0200

    Revert "Remove struct keyword"
    This reverts commit d3c91b11b2547cc9e394aa294e8f5d5f35646576.

--- src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
+++ src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cpp
@@ -116,7 +116,7 @@ namespace {
 	}
 
 	template<typename T>
-	void freeCommonArgumentsKernel(Program<T>::CommonKernelParameters &commonParameters)
+	void freeCommonArgumentsKernel(struct Program<T>::CommonKernelParameters &commonParameters)
 	{
 		cudaFree(commonParameters.cudaMV.data);
 		cudaFree(commonParameters.cudaClnm);
