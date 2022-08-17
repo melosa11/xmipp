cba79935b7ea810db7d6c8315712de2dbddd48dd
Jakub Kiska <kubo.kiska@gmail.com>
2022-08-17 15:45:19 +0200

    Add atomicAdd for both precision types

--- src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cu
+++ src/xmipp/libraries/reconstruction_cuda/cuda_forward_art_zernike3d.cu
@@ -278,6 +278,28 @@ namespace device {
 	}
 
 	template<typename PrecisionType>
+	__device__ PrecisionType atomicAddPrecision(PrecisionType *addr, PrecisionType val)
+	{
+		return atomicAdd(addr, val);
+	}
+
+	template<>
+	__device__ double atomicAddPrecision(double *address, double val)
+	{
+		unsigned long long int *address_as_ull = (unsigned long long int *)address;
+		unsigned long long int old = *address_as_ull, assumed;
+
+		do {
+			assumed = old;
+			old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val + __longlong_as_double(assumed)));
+
+			// Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+		} while (assumed != old);
+
+		return __longlong_as_double(old);
+	}
+
+	template<typename PrecisionType>
 	__device__ void splattingAtPos(PrecisionType pos_x,
 								   PrecisionType pos_y,
 								   PrecisionType weight,
